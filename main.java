0c097e5bb45b1f70231f6356d2e8bc3c1e311741
GreggChase <94650453+GreggChase@users.noreply.github.com>
2023-09-11 14:43:32 -0400

    why  doesn't this work? anyway its better?

--- main.java
+++ main.java
@@ -1,7 +1,21 @@
+import java.nio.charset.StandardCharsets;
+import java.security.*;
+import java.util.Scanner;
+import java.lang.Object;
+import javax.xml.bind.DatatypeConverter;
 class Encrypt{
   
     public static void main(String[] args){
-        System.out.println("Hello World");
+        System.out.println("enter memsecret");
+        Scanner scanner = new Scanner(System.in);
+String password = scanner.nextLine();
+scanner.close();
+
+MessageDigest digest = null;
+digest = MessageDigest.getInstance("SHA-256");
+byte[] hash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+String encoded = DatatypeConverter.printHexBinary(hash);        
+System.out.println(encoded.toLowerCase());
     }
 
 }
