fcb778bb9357bb051be700cb1a48ecbeb6cb14e0
GreggChase <94650453+GreggChase@users.noreply.github.com>
2023-09-17 23:29:59 -0400

    Update main.java

--- main.java
+++ main.java
@@ -1,8 +1,10 @@
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
 import java.util.Scanner;
 import javax.crypto.*;
 import javax.crypto.spec.PBEKeySpec;
-import javax.print.DocFlavor.CHAR_ARRAY;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
 class Encrypt{
     public static  char[] password(){
@@ -13,11 +15,15 @@ class Encrypt{
         return password;
     }
     public static byte[] salt(){
-        byte[] salt = FileImputStream("saltBytes.txt");
-        Scanner.close();
-        return salt;
+        try (FileInputStream FileInputStream = new FileInputStream("saltBytes.txt")) {
+            byte[] salt = FileInputStream.readAllBytes();
+            return salt;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
-    public static void main(String[] args){
+    public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeySpecException{
         char[] password = password();
         byte[] salt = salt();
         SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
