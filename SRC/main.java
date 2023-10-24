1bdf359e6c67f8afdd466a106b6cdb07aaf720f3
GreggChase <94650453+GreggChase@users.noreply.github.com>
2023-10-24 14:05:49 -0400

    added demo main method
    added method that shows steps

--- SRC/main.java
+++ SRC/main.java
@@ -7,12 +7,12 @@ import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
 
 class Encrypt{
-  IvParameterSpec ivParameterSpec;
-  String password;
-  String salt;   
+  static IvParameterSpec ivParameterSpec;
+  static String password;
+  static String salt;   
 
     
-    public  String encrypt () throws InvalidKeySpecException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException, InvalidAlgorithmParameterException, BadPaddingException, IllegalBlockSizeException {
+    public static  String encrypt () throws InvalidKeySpecException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException, InvalidAlgorithmParameterException, BadPaddingException, IllegalBlockSizeException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("enter password");
         password = scanner.nextLine();
@@ -26,11 +26,19 @@ class Encrypt{
         String cipherText = AESUtil.encryptPasswordBased(plainText, key, ivParameterSpec);
       return cipherText;
     }
-      public String decrypt(String cipherText) throws NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException, NoSuchPaddingException, InvalidAlgorithmParameterException, BadPaddingException, IllegalBlockSizeException{
+      public static String decrypt(String cipherText) throws NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException, NoSuchPaddingException, InvalidAlgorithmParameterException, BadPaddingException, IllegalBlockSizeException{
 
         SecretKey key = AESUtil.getKeyFromPassword(password,salt);
         String decryptedCipherText = AESUtil.decryptPasswordBased(
           cipherText, key, ivParameterSpec);
           return decryptedCipherText;
     }
+    public static void main(String[] args) throws InvalidKeyException, InvalidKeySpecException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException, BadPaddingException, IllegalBlockSizeException {
+      String cipherText = encrypt();
+      System.out.println(cipherText);
+      String decodedText = decrypt(cipherText);
+      System.out.println(decodedText);
+    }
+
+
   }
