c6feddc3585295371880718008cd565629b68ba9
GreggChase <94650453+GreggChase@users.noreply.github.com>
2023-10-31 14:02:48 -0400

    added gui

---
+++ SRC/MyGUI.java
@@ -0,0 +1,73 @@
+import java.awt.*;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.swing.*;
+public class MyGUI extends JFrame
+ 
+{
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton button;
+
+    public MyGUI() {
+        super("My GUI");
+
+        JPanel panel = new JPanel();
+        GridLayout gridLayout = new GridLayout(4,1);
+        panel.setLayout( gridLayout);
+
+        JLabel label1 = new JLabel("password:");
+        panel.add(label1);
+
+        textField1 = new JTextField();
+        panel.add(textField1);
+
+        JLabel label2 = new JLabel("salt:");
+        panel.add(label2);
+
+        textField2 = new JTextField();
+        panel.add(textField2);
+
+        JLabel label3 = new JLabel("plaintext:");
+        panel.add(label3);
+
+        textField3 = new JTextField();
+        panel.add(textField3);
+
+        button = new JButton("send");
+        panel.add(button);
+
+        button.addActionListener(e -> {
+            String string1 = textField1.getText();
+            String string2 = textField2.getText();
+            String string3 = textField3.getText();
+
+            // Call the method using the three strings
+            
+            Encrypt enc = new Encrypt();
+            try {
+                enc.encrypt(string1,string2,string3);
+            } catch (InvalidKeyException | InvalidKeySpecException | NoSuchAlgorithmException | NoSuchPaddingException
+                    | InvalidAlgorithmParameterException | BadPaddingException | IllegalBlockSizeException e1) {
+                
+                e1.printStackTrace();
+            }
+        });
+
+        add(panel);
+        setSize(400, 200);
+        setVisible(true);
+    }
+public static void main(String[] args) {
+    new MyGUI();
+}
+   
+    
+}
--- SRC/main.java
+++ SRC/main.java
@@ -2,35 +2,29 @@ import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.*;
-import java.util.Scanner;
 import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
 
 class Encrypt{
   IvParameterSpec ivParameterSpec;
-  String password;
   String salt;   
 
     
-    public  String encrypt () throws InvalidKeySpecException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException, InvalidAlgorithmParameterException, BadPaddingException, IllegalBlockSizeException {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("enter password");
-        password = scanner.nextLine();
-        System.out.println("enter salt");
-        salt = scanner.nextLine();
-        System.out.println("enter message");
-        String plainText = scanner.nextLine();
-        scanner.close();
+    public void encrypt (String p, String s, String plaintext) throws InvalidKeySpecException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException, InvalidAlgorithmParameterException, BadPaddingException, IllegalBlockSizeException {
+        String password = p;
+        salt =s;
+        String plainText = plaintext;
         ivParameterSpec = AESUtil.generateIv();
         SecretKey key = AESUtil.getKeyFromPassword(password,salt);
         String cipherText = AESUtil.encryptPasswordBased(plainText, key, ivParameterSpec);
-      return cipherText;
+      System.out.print(cipherText);
     }
-      public String decrypt(String cipherText) throws NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException, NoSuchPaddingException, InvalidAlgorithmParameterException, BadPaddingException, IllegalBlockSizeException{
-
+      public void decrypt(String password, String s, String cipherText) throws NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException, NoSuchPaddingException, InvalidAlgorithmParameterException, BadPaddingException, IllegalBlockSizeException{
+        String salt = s;
         SecretKey key = AESUtil.getKeyFromPassword(password,salt);
         String decryptedCipherText = AESUtil.decryptPasswordBased(
           cipherText, key, ivParameterSpec);
-          return decryptedCipherText;
+          System.out.print(decryptedCipherText);
     }
+
   }
